07ed492901e09f8b66c09dfef3e4788fdc97671c
RawanAlakhras <ralakhras36@gmail.com>
2021-06-16 15:33:09 +0300

    complete lab 09

--- src/componant/Weather.js
+++ src/componant/Weather.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-class Weather extends React.Component{
-render(){
-    return (
-        
-        
-           <li>{this.props.date} {this.props.description}</li>
-        
-    
-    )
+import WeatherDay from './WeatherDay';
+class Weather extends React.Component {
+    render() {
+        return (
+            <>
+                {this.props.weatherArr.map((item, inx) => {
+                    return (
+                        <WeatherDay date={item.date} description={item.description} key={inx.toString()} />
+                    )
+                }
+                )
+                }
+            </>
+        )
 
-}
+    }
 }
 export default Weather;
